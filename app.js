72350d060c2bf14a4f5252bf9a77d2bc918e63c4
Tiago Ferreira <ferreiratiago@outlook.com>
2024-08-01 15:11:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -66,16 +66,14 @@ app.get('/about_new', routes.about_new);
 app.get('/chat', routes.chat.get);
 app.put('/chat', routes.chat.add);
 app.delete('/chat', routes.chat.delete);
-app.use('/users', routesUsers)
+app.use('/users', routesUsers);
 
 // Static
 app.use(st({ path: './public', url: '/public' }));
 
-// Add the option to output (sanitized!) markdown
 marked.setOptions({ sanitize: true });
 app.locals.marked = marked;
 
-// development only
 if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
